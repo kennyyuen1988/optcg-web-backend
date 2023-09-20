382819e20ffe509d9f47cfd25da3e6a584df0914
Kenny Yuen <kennyyuen@KennydeMacBook-Pro.local>
2023-09-21 00:13:57 +0800

    chore:aadd cors

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-require('dotenv').config();
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
 
-const express = require('express');
-const { Pool } = require('pg');
+require("dotenv").config();
+
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3721", "https://optcg-web.vercel.app"],
+  })
+);
+
 const port = 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-app.get('/api/data', async (req, res) => {
+app.get("/api/data", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM cards where id=1');
-    const results = { 'results': (result) ? result.rows : null};
+    const result = await client.query("SELECT * FROM cards where id=1");
+    const results = { results: result ? result.rows : null };
     res.send(results);
     client.release();
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
